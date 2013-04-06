e1b7d9eb155b914b7c072bc5ce3d6b963290e6d5
Daniel Malament <daniel.j.malament@gmail.com>
2013-04-06 15:05:03 -0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+case "$1" in
+  system)
+    cp aeolus-lib.sh /usr/local/lib/
+    chmod 755 /usr/local/lib/aeolus-lib.sh
+    ;;
+  user)
+    mkdir -p ~/bin
+    chmod u=rwx ~/bin
+    cp aeolus-lib.sh ~/bin/
+    chmod u=rwx ~/bin/aeolus-lib.sh
+    ;;
+  *)
+    cat 1>&2 <<-EOF
+
+	Usage:
+
+	  $0 { system | user }
+
+	"system" installs systemwide;
+	"user" installs to the home directory of the current user
+
+	EOF
+    ;;
+esac
